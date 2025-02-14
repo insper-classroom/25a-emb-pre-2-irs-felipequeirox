821ff1b73a7dd24ca177bcd7489a33674f3ba2e0
Felipe <fepodqueiroz@gmail.com>
2025-02-14 19:05:54 -0300

    Teste 2.4

--- exe2/main.c
+++ exe2/main.c
@@ -4,7 +4,7 @@
 
 const int BTN_PIN_R = 28;
 const int LED_PIN = 4;
-volatile int btn_flag;
+volatile btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
@@ -27,11 +27,12 @@ int main() {
 
 
   gpio_set_irq_enabled_with_callback(
-      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+      BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
     if (btn_flag) {
       gpio_put(LED_PIN, 1);
+      btn_flag = 0;
     } else {
       gpio_put(LED_PIN, 0);
     }
