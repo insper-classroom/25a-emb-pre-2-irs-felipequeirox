c1be8345f445d04d0fefdb0fd89a85577835f59c
Felipe <fepodqueiroz@gmail.com>
2025-02-16 10:35:06 -0300

    Teste 2.5

--- exe2/main.c
+++ exe2/main.c
@@ -4,13 +4,15 @@
 
 const int BTN_PIN_R = 28;
 const int LED_PIN = 4;
-volatile btn_flag;
+volatile int btn_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    btn_flag = 1;
-  } else if (events == 0x8) { // rise edge
-    btn_flag = 0;
+    if (btn_flag == 0){
+      btn_flag = 1;
+    } else {
+      btn_flag = 0;
+    }
   }
 }
 
@@ -30,12 +32,7 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
-    if (btn_flag) {
-      gpio_put(LED_PIN, 1);
-      btn_flag = 0;
-    } else {
-      gpio_put(LED_PIN, 0);
-    }
+    gpio_put(LED_PIN, btn_flag);
     sleep_ms(1000);
   }
 }
